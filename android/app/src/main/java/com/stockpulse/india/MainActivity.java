2bb7058c755112a912fbcba391d7c995167519ac
raymaljalu7-ctrl <raymaljalu7@gmail.com>
2026-09-12 13:42:23 +0530

    Fix Android app to load full v3.3 screener UI

--- android/app/src/main/java/com/stockpulse/india/MainActivity.java
+++ android/app/src/main/java/com/stockpulse/india/MainActivity.java
@@ -8,18 +8,20 @@ import android.webkit.WebViewClient;
 import android.graphics.Color;
 
 public class MainActivity extends Activity {
-    private static final String APP_URL = "https://stockpulse-india-web.onrender.com";
+    private static final String APP_URL = "https://stockpulse-india-web.onrender.com/?app=3.3";
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView w = findViewById(R.id.webview);
-        w.setBackgroundColor(Color.WHITE);
+        w.setBackgroundColor(Color.rgb(8,16,29));
         w.setWebViewClient(new WebViewClient());
         WebSettings s = w.getSettings();
         s.setJavaScriptEnabled(true);
         s.setDomStorageEnabled(true);
+        s.setCacheMode(WebSettings.LOAD_NO_CACHE);
         s.setLoadWithOverviewMode(true);
         s.setUseWideViewPort(true);
+        w.clearCache(true);
         w.loadUrl(APP_URL);
     }
     @Override public void onBackPressed() {
